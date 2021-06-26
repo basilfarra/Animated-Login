ec8f542f22d126bfb5ec2f74e8b6337df88cbc44
Basilfarra <40392957+basilfarra@users.noreply.github.com>
2021-06-26 18:57:12 +0300

    Add files via upload

---
+++ Animated Login/src/com/bennycoder/main/Launch.java
@@ -0,0 +1,34 @@
+package com.bennycoder.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Benny
+ */
+public class Launch extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));        
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ Animated Login/src/com/bennycoder/main/MainController.java
@@ -0,0 +1,79 @@
+package com.bennycoder.main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author Benny
+ */
+public class MainController implements Initializable {
+
+    @FXML
+    private VBox vbox;
+
+    private Parent fxml;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(vbox.getLayoutX() * 32);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+
+    @FXML
+    private void open_signup(ActionEvent event) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(14);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        });
+    }
+
+    @FXML
+    private void open_signin(ActionEvent event) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(vbox.getLayoutX() * 32);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        });
+
+    }
+
+}
